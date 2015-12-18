51340f926fe2e7cfa1e0e246a7f3d9c6bf5110d6
Damjan Georgievski <gdamjan@gmail.com>
2015-12-18 19:38:17 +0100

    introduce rpc.py

---
+++ rpc.py
@@ -0,0 +1,19 @@
+from itsdangerous import Serializer
+from werkzeug.http import parse_cookie
+from uwsgidecorators import rpc
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
+AUTH_COOKIE = os.environ['AUTH_COOKIE']
+
+@rpc('authrpc')
+def authrpc(http_cookie):
+    cookies = parse_cookie(http_cookie)
+    cookie = cookies.get(AUTH_COOKIE)
+    if cookie is None:
+        return b'no cookie'
+    s = Serializer(SECRET_KEY)
+    payload = s.loads(cookie)
+    if payload.get('authorized') == True:
+       return b'OK'
+    return b'no authorization'
